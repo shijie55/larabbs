d85c08ef4027b5fb36dde4ab4716b7b38f2c9127
shi <northfox11@163.com>
2018-02-27 08:49:44 +0000

    policy add

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,9 +13,10 @@ class UsersController extends Controller
 
     public function __construct()
     {
+        $this->middleware('auth', ['except' => ['show']]);
         $this->imageUp = new ImageUploadHandler();
     }
-    
+
     public function show(User $user)
     {
         return view('users.show', compact('user'));
@@ -23,11 +24,13 @@ class UsersController extends Controller
 
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
