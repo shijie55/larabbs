b75925728f1cb35e2b5cd3bc720a78ef3070e7c9
shi <northfox11@163.com>
2018-02-28 03:07:25 +0000

    user can create topic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
 	{
+	    //事件监听器注册
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
 
