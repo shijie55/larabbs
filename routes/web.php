7685eabce7038d1f12b813a39e3ca5c6bfd04d62
shi <northfox11@163.com>
2018-02-28 01:57:46 +0000

    category topic list

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::resource('users', 'UsersController');
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController');
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
