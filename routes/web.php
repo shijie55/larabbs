8842a0980675d924544feee78963b93ae2dca6e0
shi <northfox11@163.com>
2018-02-28 00:52:58 +0000

    generated topics

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes();
 
 Route::resource('users', 'UsersController');
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
